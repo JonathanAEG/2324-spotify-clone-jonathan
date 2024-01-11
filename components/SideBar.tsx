1b7ddf3fb75240ff2362fc622a9a9229a9c6e701
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-01-11 12:50:51 +0100

    [ADD] Implementada reproducción de las canciones que se cargan en la pantalla home

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -8,6 +8,8 @@ import Box from "./Box";
 import SideBarItem from "./SideBarItem";
 import Library from "./Library";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 
 interface SideBarProps{
@@ -18,6 +20,7 @@ interface SideBarProps{
 const SideBar: React.FC<SideBarProps> = ({children, songs})=>{
 
     const pathname = usePathname();
+    const player = usePlayer();
 
     const routes = useMemo(() => [
         {
@@ -36,7 +39,7 @@ const SideBar: React.FC<SideBarProps> = ({children, songs})=>{
 
 
     return(
-        <div className="flex h-full">
+        <div className={twMerge(`flex h-full`, player.activeId && "h-[calc(100%-80px)]")}>
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
                     <div className="flex flex-col gap-y-4 px-5 py-4">
